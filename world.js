b09ec069a765a7d82181a4b86df3d19025cf5dcf
Jamilla Morgan <jamillamorgan02@gmail.com>
2022-11-14 23:44:20 -0500

    Completed Part of Exercise 5

--- world.js
+++ world.js
@@ -1,14 +1,34 @@
 window.addEventListener("load", function (e){
-    var btn = document.querySelector("button");
+    var lookupBtn = document.querySelector("button");
+    var citiesBtn = document.getElementById("city");
+    const result = document.querySelector("#result");
+    const input = document.querySelector("input");
 
-    btn.addEventListener('click', function (e) {
+    lookupBtn.addEventListener('click', function (e) {
         e.preventDefault();
         
-        const result = document.querySelector("#result");
-        const input = document.querySelector("input");
+        // const result = document.querySelector("#result");
+        // const input = document.querySelector("input");
 
-        let country = input.value.trim();
-        let url = `world.php?country=${country}`;
+        //let country = input.value.trim();
+        let url = 'world.php?country=' + input + '&lookup=countries';
+
+        fetch(url)
+            .then(response => {
+                return response.text();
+            })
+
+            .then(data => {
+                result.innerHTML = data;
+                console.log(data);
+            })
+
+    });
+
+    citiesBtn.addEventListener('click', function (e) {
+        e.preventDefault();
+
+        let url = 'world.php?country=' + input + '&lookup=cities';
 
         fetch(url)
             .then(response => {
--- world.php
+++ world.php
@@ -7,8 +7,10 @@ $country = $_GET['country'];
 
 $conn = new PDO("mysql:host=$host;dbname=$dbname;charset=utf8mb4", $username, $password);
 $stmt = $conn->query("SELECT * FROM countries WHERE name LIKE '%$country%'");
+$city= $conn->query("SELECT cities.name, cities.district, cities.population FROM countries join cities on cities.country_code = countries.code WHERE countries.name LIKE '%$country'"); 
 
 $results = $stmt->fetchAll(PDO::FETCH_ASSOC);
+$cities= $city->fetchAll(PDO::FETCH_ASSOC);
 
 ?>
 <table>
@@ -32,14 +34,33 @@ $results = $stmt->fetchAll(PDO::FETCH_ASSOC);
 </table>
 
 <?php
-if ($_SERVER['REQUEST_METHOD'] == 'GET') {
-  $country = filter_input(INPUT_GET, 'country', FILTER_SANITIZE_STRING);
+// if ($_SERVER['REQUEST_METHOD'] == 'GET') {
+//   $country = filter_input(INPUT_GET, 'country', FILTER_SANITIZE_STRING);
 
-  if(empty($country)) {
-    foreach ($results as $result) {
-      echo $result['name']; 
+  if($_GET['lookup'] == 'countries') {
+    if(empty($country)) {
+      foreach ($results as $result) {
+        echo $result['name']; 
+      }
     }
   }
-  
-}
+
+  if($_GET['lookup']== "cities"){
+    // echo "<table>
+    //   <thead>
+    //     <tr>
+    //       <th>Name</th>
+    //       <th>District</th>
+    //       <th>Population</th>
+    //     </tr>
+    //   </thead>
+    //   <tbody>";
+        foreach ($cities as $row):
+          echo "<table><tr><th>Name</th><th>District</th><th>Population</th></tr>";
+        foreach($cities as $row):
+          echo "<tr><td>".$row["name"]."</td><td>".$row["district"]."</td><td>".$row["population"]."</td></tr>";
+        endforeach;
+        echo "</table>";
+
+  }
 ?>
